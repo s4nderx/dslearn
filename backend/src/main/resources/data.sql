7bf1646ce9efe8a362bf72498ed3d687cdc976f2
s4nderx <phellipe.sander1@gmail.com>
2021-01-13 22:21:54 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) values ('Bootcamp HTML', 'https://image.freepik.com/vetores-gratis/jovem-professor-dos-desenhos-animados-homem-professor-na-lousa-ensinando-as-criancas-na-ilustracao-em-vetor-escola-sala-de-aula_53562-7922.jpg', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Encyclopedia_icon.svg/1200px-Encyclopedia_icon.svg.png');
