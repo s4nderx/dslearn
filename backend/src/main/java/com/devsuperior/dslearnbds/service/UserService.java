7e94f4e692b1f7e85f3a8f7148702784c9ea0ee4
s4nderx <phellipe.sander1@gmail.com>
2021-01-15 01:22:12 -0300

    OAuth, security e login testado

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/service/UserService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dslearnbds.service;
+
+import com.devsuperior.dslearnbds.entities.User;
+import com.devsuperior.dslearnbds.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private static Logger log = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByEmail(username);
+        if(null == user){
+            log.error("User not found {}", username);
+            throw new UsernameNotFoundException("Email not found.");
+        }
+        log.info("User found: {}", username);
+        return user;
+    }
+
+}
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO tb_user (name, email, password) VALUES ('Alex Sander', 'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
-INSERT INTO tb_user (name, email, password) VALUES ('Bob Singer', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Bob Singer', 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 INSERT INTO tb_user (name, email, password) VALUES ('Maria Helena', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_STUDENT');
